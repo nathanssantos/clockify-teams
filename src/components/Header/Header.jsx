82f9d4a76d31c0d7bb18b8c7c919122852c5b4df
Nathan Silva Santos <nathan@w16.com.br>
2022-02-25 18:19:56 -0300

    header hide effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import { observer } from 'mobx-react';
@@ -10,8 +10,39 @@ import Logo from '../Logo/Logo';
 import './styles.scss';
 
 const Header = () => {
+  const [headerShown, setHeaderShown] = useState(true);
+  let lastScrollTop = 0;
+
+  const detectScrollDirection = () => {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > lastScrollTop) {
+      setHeaderShown(false);
+    } else {
+      setHeaderShown(true);
+    }
+
+    lastScrollTop = st <= 0 ? 0 : st;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', detectScrollDirection, false);
+
+    return () => {
+      window.removeEventListener('scroll', detectScrollDirection, false);
+    };
+  }, []);
   return (
-    <AppBar className="header" position="sticky">
+    <AppBar
+      className="header"
+      position="sticky"
+      style={{
+        transform: headerShown
+          ? 'none'
+          : 'translate3d(0, calc(-100% - 2rem),0)',
+        transition: 'transform 0.25s cubic-bezier(0.645,0.045,0.355,1)',
+      }}
+    >
       <Container maxWidth="lg">
         <div className="header__content">
           <Logo />
